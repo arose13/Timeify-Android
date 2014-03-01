b40fc038238dafdc73140a17aef3e16241c16924
arose13 <anthony_rose12@hotmail.com>
2014-03-01 14:40:28 -0500

    Initial Setup

--- src/ca/timeify/android/MainView.java
+++ src/ca/timeify/android/MainView.java
@@ -2,7 +2,6 @@ package ca.timeify.android;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class MainView extends Activity {
 
@@ -10,13 +9,6 @@ public class MainView extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_view);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_view, menu);
-		return true;
 	}
 
 }
