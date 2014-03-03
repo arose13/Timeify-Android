0c0e6f97d357d88dc27fa127e11c5b639d2e7512
arose13 <anthony_rose12@hotmail.com>
2014-03-03 14:48:10 -0500

    SplashView init

--- gen/ca/timeify/android/R.java
+++ gen/ca/timeify/android/R.java
@@ -64,6 +64,7 @@ public final class R {
     }
     public static final class layout {
         public static final int main_view=0x7f030000;
+        public static final int splash_view=0x7f030001;
     }
     public static final class menu {
         public static final int main_view=0x7f080000;
--- src/ca/timeify/android/views/SplashView.java
+++ src/ca/timeify/android/views/SplashView.java
@@ -1,7 +1,15 @@
 package ca.timeify.android.views;
 
+import android.os.Bundle;
+import ca.timeify.android.R;
 import ca.timeify.android.activities.BaseActivity;
 
 public class SplashView extends BaseActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_view);
+	}
 
 }
