ef5175351af321c43dfeb20ac45016c45c60098b
arose13 <anthony_rose12@hotmail.com>
2014-03-04 23:47:35 -0500

    emptied SplashView class

--- src/ca/timeify/android/views/SplashView.java
+++ src/ca/timeify/android/views/SplashView.java
@@ -1,10 +1,8 @@
 package ca.timeify.android.views;
 
 import android.os.Bundle;
-import android.widget.TextView;
 import ca.timeify.android.R;
 import ca.timeify.android.activities.BaseActivity;
-import ca.timeify.android.data.Fonts;
 
 public class SplashView extends BaseActivity {
 	
